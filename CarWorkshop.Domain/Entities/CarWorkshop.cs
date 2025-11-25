d0718ae9dcd995805dabd7545ed551b256d3896f
Janek <gornada@gmail.com>
2025-11-25 18:21:18 +0100

    [2.14-17] clean architecture i modele

---
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -0,0 +1,11 @@
+﻿namespace CarWorkshop.Domain.Entities
+{
+    class CarWorkshop
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+        public string EncodedName { get; private set; } = default!;
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ","-")
+}
